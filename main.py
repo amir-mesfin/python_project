254a8c6d3a0a1dd14bc513a90ec734ef186c775c
abushe136 <amir.mesfin136@gmail.com>
2025-11-21 01:06:12 +0300

    Add UI elements for city input and weather display

--- main.py
+++ main.py
@@ -9,7 +9,10 @@ from PyQt5.QtCore import Qt
 class WeatherApp(QWidget):
   def __init__(self):
     super().__init__()
-    
+    self.city_label = QLabel("Enter City Name :", self)
+    self.city_input = QLineEdit(self)
+    self.get_weather_button = QPushButton("Get Wether", self)
+    self.temperature_label = QLabel("45")
     
     
 if __name__ == "__main__":
