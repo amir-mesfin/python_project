fa70dd26d971049859057b1bdba4922371551ecc
abushe136 <amir.mesfin136@gmail.com>
2025-11-21 21:23:34 +0300

    Enhance WeatherApp UI with temperature unit, emoji, and description labels

--- main.py
+++ main.py
@@ -12,8 +12,22 @@ class WeatherApp(QWidget):
     self.city_label = QLabel("Enter City Name :", self)
     self.city_input = QLineEdit(self)
     self.get_weather_button = QPushButton("Get Wether", self)
-    self.temperature_label = QLabel("45")
+    self.temperature_label = QLabel("45 f", self)
+    self.emoji_label = QLabel("", self)
+    self.description_label = QLabel("Sunny", self)
+    self.initUI()
     
+  def initUI(self):
+    self.setWindowTitle("Weather App")
+    
+    vbox = QVBoxLayout()
+    
+    vbox.addWidget(self.city_label)
+    vbox.addWidget(self.city_input)
+    vbox.addWidget(self.get_weather_button)
+    vbox.addWidget(self.temperature_label)
+    vbox.addWidget(self.emoji_label)
+    vbox.addWidget(self.description_label)
     
 if __name__ == "__main__":
    app = QApplication(sys.argv)
