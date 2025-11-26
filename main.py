981663a857b0171dcf4a4a27eef566b28b197ba0
abushe136 <amir.mesfin136@gmail.com>
2025-11-26 15:42:32 +0300

    weather app

--- main.py
+++ main.py
@@ -139,20 +139,47 @@ class WeatherApp(QWidget):
   def display_error(self, message):
     # print(message)
     self.temperature_label.setText(message)
+    self.emoji_label.clear()
+    self.description_label.clear()
   def display_weather(self,data):
     # print(data)
     temperature_k = data["main"]["temp"]
     temperature_c = temperature_k - 273.15
     temperature_f = (temperature_k * 9/5) - 459.67
-    
     self.temperature_label.setText(f"{temperature_f:.0f}°F")
     
     weather_description = data["weather"][0]["description"]
-    
     self.description_label.setText(weather_description)
+    
+    weather_id = data["weather"][0]["id"]
+    self.emoji_label.setText(self.get_weather_emoji(weather_id))
   @staticmethod
   def get_weather_emoji(weather_id):
-    pass
+        if 200 <= weather_id <= 232:         
+            return '⛈️'
+        elif 300 <= weather_id <= 321:       
+            return '🌦️'
+        elif 500 <= weather_id <= 531:       
+            return '🌧️'
+        elif 600 <= weather_id <= 622:       
+            return '❄️'
+        elif 700 <= weather_id <= 741:       
+            return '🌫️'
+        elif weather_id == 762:              
+            return '🌋'
+        elif 751 <= weather_id <= 761:      
+            return '🌪️'
+        elif weather_id == 781:         
+            return '🌪️'
+        elif weather_id == 800:              
+            return '☀️'
+        elif 801 <= weather_id <= 804:      
+            return '☁️'
+        else:
+            return '🌍'   
+
+    
+    
 if __name__ == "__main__":
    app = QApplication(sys.argv)
    WeatherApp_app = WeatherApp()
